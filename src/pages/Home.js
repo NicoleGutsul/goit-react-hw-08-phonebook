1d1aaed40ee4c796976c0b124f8883cda2e011f2
Nicole Gutsul <ijustnicole@gmail.com>
2022-10-16 21:57:38 +0100

    creates Home page

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import { Box } from "components/Box";
+
+export default function Home() {
+    return (
+        <Box>
+            <h1>
+                Hello, it's your phonebook, welcome page{' '}
+                <span role="img" aria-label="Greeting icon">
+                    💁‍♀️
+                </span>
+            </h1>
+        </Box>
+    );
+}
