053efb5489faae54307ddc061506319599de934e
Nicole Gutsul <ijustnicole@gmail.com>
2022-10-16 21:52:56 +0100

    creates Toast

---
+++ src/components/Toast/Toast.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+function Toast(){
+    const notify = () => toast("contact add!");
+
+    return (
+        <div>
+            {notify}
+            <ToastContainer />
+        </div>
+    );
+}
+
+export default Toast;
