ec6069bb40f03c29cdde846be3a6cfbad0915541
Nicole Gutsul <ijustnicole@gmail.com>
2022-10-16 22:00:54 +0100

    creates Register page

---
+++ src/pages/Register.js
@@ -0,0 +1,14 @@
+import { Helmet } from 'react-helmet';
+import { RegisterForm } from 'components/RegisterForm/RegisterForm';
+import { Box } from 'components/Box';
+
+export default function Register() {
+    return (
+        <Box>
+            <Helmet>
+                <title>Registration</title>
+            </Helmet>
+            <RegisterForm />
+        </Box>
+    );
+}
