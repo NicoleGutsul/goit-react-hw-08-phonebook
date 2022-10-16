6a21736d20c81699c8e28d645d54f9a40eb54162
Nicole Gutsul <ijustnicole@gmail.com>
2022-10-16 21:44:56 +0100

    creates AppBar

---
+++ src/components/AppBar/AppBar.styled.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+    margin-top: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+`;
